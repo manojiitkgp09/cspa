67e9dc21b5d4c83ed5477908915f987be31fb380
Manoj <manoj.agarwal@pinogy.com>
2016-12-16 14:15:58 +0530

    Add html js

---
+++ week1/app.js
@@ -0,0 +1,33 @@
+(function(){
+	angular.module("LunchCheck", [])
+	.controller("LunchCheckController", LCC);
+
+	LCC.$inject = ['$scope'];
+	
+	function LCC($scope){
+		$scope.items = "";
+		$scope.msgColor = "black";
+		$scope.lunchStatusMessage = "";
+
+		$scope.checkItems = function(){
+			$scope.lunchStatusMessage = "Please enter data first";
+			$scope.msgColor = "red";
+			var items = $scope.items.split(",");
+			var count = 0
+			for (var i=0;i<items.length;i++){
+				if (items[i].trim() != ""){
+					count++;
+				}
+			}
+			if (count > 0) {
+				$scope.msgColor = "green";
+				if (count <=3){
+					$scope.lunchStatusMessage = "Enjoy!";
+				} else{
+					$scope.lunchStatusMessage = "Too much!";
+				}
+			}
+		}
+	}
+	
+})();
